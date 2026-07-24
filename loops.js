3eb956a9c9986b6cbbefcee81f2c510947410346
olarewajugabriel <gabriel75-ai.com>
2026-07-24 13:16:11 -0700

    Added loops.js

---
+++ loops.js
@@ -0,0 +1,14 @@
+// Task 1: Odd numbers 1 to 20
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// Task 2: Best friends array
+let friends = ["Gabriel", "Alex", "John", "Felix", "Sam"];
+console.log(friends);
+
+// Task 3: Best friend selection
+let bestFriend = friends[0];
+console.log(bestFriend);
